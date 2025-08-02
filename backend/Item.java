6ae7a59fe152f4f86d4c52fcb40dda5f02236860
aroyasr <arohakiri@gmail.com>
2025-08-02 17:29:30 +1200

    Add item class

---
+++ backend/Item.java
@@ -0,0 +1,67 @@
+/*
+ * 2025 Freshies
+ * Item.java
+ * This Java file specifies how data is stored for each item in the app.
+ */
+
+import java.nio.file.Path;
+
+public class Item {
+    /* Fields */
+    private int position;
+    private string name;
+    private string caption;
+    private Path image;
+    private int stars;
+
+    /* Constructor */
+    public Item(int position, string name, string caption, Path path, int stars){
+        this.position = position;
+        this.name = name;
+        this.caption = caption;
+        this.path = path;
+        this.stars = stars;
+    }
+
+    /* Methods: Getters */
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public Path getImage() {
+        return image;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    /* Methods: Setters */
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public void setImage(Path image) {
+        this.image = image;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+}
--- backend/myCollection.java
+++ backend/myCollection.java
@@ -31,6 +31,7 @@ public class myCollection {
         this.col2 = c2;
         this.icon = icon;
     }
+    
     /* Methods */
     /**
      * addItem
@@ -40,6 +41,19 @@ public class myCollection {
      */
     public void addItem(Item item){
         items.add(item);
+    }
+    /**
+     * addItem
+     * input: index, item
+     * output: n/a
+     * adds the given item to the specified location in the items list
+     */
+    public void addItem(int index, Item item){
+        this.items.add(index, item);
+    }
+
+    public void removeItem(Item item){
+        this.items.remove(item);
     }
 
     /**
@@ -52,4 +66,62 @@ public class myCollection {
         this.starCount++;
     }
 
+    /* Methods: Getters */
+    public string getcollectionID(){
+        return this.collectionID;
+    }
+    public string getOwnerUID(){
+        return this.ownerUID;
+    }
+    public string getName(){
+        return this.name;
+    }
+    public string getDescription(){
+        return this.description;
+    }
+    public int getStarCount(){
+        return this.starCount;
+    }
+    public Color getColor(int index){
+        if(index == 1)
+            return col1;
+        else if(index == 2)
+            return col2;
+        return null;
+    }
+    public ArrayList<Item> getItems(){
+        return this.items;
+    }
+
+    /* Methods: Setters */
+    public void setCollectionID(String collectionID){
+        this.collectionID = collectionID;
+    }
+
+    public void setOwnerUID(String ownerUID){
+        this.ownerUID = ownerUID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setStarCount(int starCount){
+        this.starCount = starCount;
+    }
+
+    public void setColor(int index, Color color){
+        if(index == 1)
+            this.col1 = color;
+        else if(index == 2)
+            this.col2 = color;
+    }
+
+    public void setItems(ArrayList<Item> items){
+        this.items = items;
+    }
 }
